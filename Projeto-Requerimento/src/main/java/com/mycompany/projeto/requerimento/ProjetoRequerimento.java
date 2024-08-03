5d1b514f231297d191c582a840e18dbe92ca7a03
ArthurHassimyan <arthurhassimyan@alunos.utfpr.edu.br>
2024-08-03 10:21:50 -0300

    inicialização

---
+++ Projeto-Requerimento/src/main/java/com/mycompany/projeto/requerimento/ProjetoRequerimento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto.requerimento;
+
+/**
+ *
+ * @author Arthur Hassimyan
+ */
+public class ProjetoRequerimento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
