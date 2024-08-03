14e8ef3e7c4c4a74618460c027d1ddfd3534a095
ArthurHassimyan <arthurhassimyan@alunos.utfpr.edu.br>
2024-08-03 15:30:38 -0300

    model

---
+++ Projeto-Requerimento/src/main/java/model/enderecoRural.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Arthur Hassimyan
+ */
+public class enderecoRural {
+    private String cidade;
+    private String fazenda;
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getFazenda() {
+        return fazenda;
+    }
+
+    public void setFazenda(String fazenda) {
+        this.fazenda = fazenda;
+    }
+}
---
+++ Projeto-Requerimento/src/main/java/model/requerente.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Arthur Hassimyan
+ */
+public class requerente extends enderecoUrbano{
+    private String nome;
+    private String estadoCivil;
+    private String profissao;
+    private int cpf;
+    private String pai;
+    private String mae;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPai() {
+        return pai;
+    }
+
+    public void setPai(String pai) {
+        this.pai = pai;
+    }
+
+    public String getMae() {
+        return mae;
+    }
+
+    public void setMae(String mae) {
+        this.mae = mae;
+    }
+}
