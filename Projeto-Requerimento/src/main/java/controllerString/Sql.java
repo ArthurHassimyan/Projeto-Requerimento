467de3bff3e5d3ecf0b80f22f22a5f6585ea49df
ArthurHassimyan <arthurhassimyan@alunos.utfpr.edu.br>
2024-08-04 15:08:31 -0300

    slq 2

--- Projeto-Requerimento/src/main/java/controllerString/Sql.java
+++ Projeto-Requerimento/src/main/java/controllerString/Sql.java
@@ -13,7 +13,7 @@ public final class Sql {
     private final static String baseProperties = base + "/src/main/java/properties";
     public final static String bdProperties = baseProperties + "/configBD.properties";
     
-    public final static String insertAgri = "INSERT INTO agrimemsor (cpf, nome, nome_doc, crea, rua, numero, bairro, cep, cidade, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    public final static String insertAgri = "INSERT INTO agrimemsor (cpf, nome, nome_doc, crea, estado_civil, rua, numero, bairro, cep, cidade, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     public final static String insertReque = "INSERT INTO agrimemsor (cpf, nome, estadoCivil, profissao, pai, mae, rua, numero, bairro, cep, cidade, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     
 }
